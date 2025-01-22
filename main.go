1d7b85bb5a8ed519335c8269c85e41bca5043970
yuichi.oba <yuichi.oba@sorich.jp>
2025-01-22 22:10:46 +0900

    add middleware

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	// ミドルウェアを設定
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Vercel!")
