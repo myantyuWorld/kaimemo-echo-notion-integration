07691e670d7c074ba8b15c817e3d28c12eb095d6
yuichi.oba <yuichi.oba@sorich.jp>
2025-01-22 21:27:16 +0900

    init golang echo

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Vercel!")
+	})
+
+	// Vercel が使用するポートを環境変数から取得
+	port := "3000"
+	e.Logger.Fatal(e.Start(":" + port))
+}
