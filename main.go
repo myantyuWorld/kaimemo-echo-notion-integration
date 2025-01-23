42893246d17e9966cdc574777804a0dc05c10f83
yuichi.oba <yuichi.oba@sorich.jp>
2025-01-23 22:20:03 +0900

    add notionapi library

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/jomei/notionapi"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -12,6 +17,50 @@ func main() {
 	// ミドルウェアを設定
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	apiKey := os.Getenv("NOTION_API_KEY")
+	databaseID := os.Getenv("NOTION_DATABASE_ID")
+
+	client := notionapi.NewClient(notionapi.Token(apiKey))
+	query := &notionapi.DatabaseQueryRequest{}
+
+	resp, err := client.Database.Query(context.Background(), notionapi.DatabaseID(databaseID), query)
+	if err != nil {
+		log.Fatalf("failed to notion query database: %v", err)
+	}
+
+	fmt.Println("---")
+	for _, result := range resp.Results {
+		properties := result.Properties
+		for key, property := range properties {
+			// テキストデータを取得
+			switch prop := property.(type) {
+			case *notionapi.TitleProperty:
+				fmt.Printf("  %s (Title): ", key)
+				for _, text := range prop.Title {
+					fmt.Print(text.Text.Content)
+				}
+				fmt.Println()
+			case *notionapi.RichTextProperty:
+				fmt.Printf("  %s (RichText): ", key)
+				for _, text := range prop.RichText {
+					fmt.Print(text.Text.Content)
+				}
+				fmt.Println()
+			case *notionapi.SelectProperty:
+				fmt.Printf("  %s (Select): %s\n", key, prop.Select.Name)
+			case *notionapi.MultiSelectProperty:
+				fmt.Printf("  %s (MultiSelect): ", key)
+				for _, option := range prop.MultiSelect {
+					fmt.Printf("%s ", option.Name)
+				}
+				fmt.Println()
+			default:
+				fmt.Printf("  %s: Unhandled property type\n", key)
+			}
+		}
+		fmt.Println("---")
+	}
 
 	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Vercel!")
